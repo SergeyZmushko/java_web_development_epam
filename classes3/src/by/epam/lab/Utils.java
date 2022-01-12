d8c27951aac90755955756cc045448dd09b89dd7
Siarhei Zmushka <szmushko83@gmail.com>
2022-01-12 16:54:22 +0300

    classes3_1

--- classes3/src/by/epam/lab/Utils.java
+++ classes3/src/by/epam/lab/Utils.java
@@ -5,7 +5,7 @@ public class Utils {
         return String.format("%d.%02d", penny / 100, penny % 100);
     }
 
-    public static String withThreeDigitsAfterThePoint(int penny){
+    public static String withThreeDigitsAfterThePoint(int penny) {
         return String.format("%d.%02d%d", penny / 100, penny % 100, 0);
     }
 }
