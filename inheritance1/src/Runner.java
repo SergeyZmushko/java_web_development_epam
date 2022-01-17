2f8e35676f6c7a074bb7bc199c65c643bb167785
Siarhei Zmushka <szmushko83@gmail.com>
2022-01-17 21:57:39 +0300

    inheritance1_1

--- inheritance1/src/Runner.java
+++ inheritance1/src/Runner.java
@@ -2,6 +2,7 @@ import by.epam.lab.*;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Runner {
@@ -9,6 +10,7 @@ public class Runner {
         final int PURCHASE_NUMBER = 6;
         Purchase[] purchases = new Purchase[PURCHASE_NUMBER];
         try (Scanner sc = new Scanner(new FileReader("src/in.txt"))) {
+            sc.useLocale(Locale.ENGLISH);
             Purchase purchaseMaxCost = new Purchase(null, new Byn(0), 0);
             int result = 1;
             for (int i = 0; i < purchases.length; i++) {
