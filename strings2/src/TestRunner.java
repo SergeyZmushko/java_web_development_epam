93e8f150e516307f1e24112b9861a3fda9ec3711
Siarhei Zmushka <szmushko83@gmail.com>
2022-02-14 19:37:35 +0300

    strings2_1

--- strings2/src/TestRunner.java
+++ strings2/src/TestRunner.java
@@ -1,59 +1,99 @@
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.Enumeration;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class TestRunner {
-    private static final String RESULT = "sum = ";
     private static final String VALUE = "value";
     private static final String INDEX = "index";
     private static final String REGEX_FOR_INDEX_WORD = "(index).*";
     private static final String REGEX_FOR_INDEX_LINE = "(index)([1-9]\\d*\\s*)";
     private static final String REGEX_FOR_INDEX_VALUE = "\\s*[1-9]\\d*\\s*";
-    private static final String REGEX_FOR_NUMBER = "\\d+";
-    private static final String FILE_NAME = "in1";
 
-    private static int getResult() throws FileNotFoundException {
-        ResourceBundle rb = ResourceBundle.getBundle(FILE_NAME);
+    private static Result getResult(String fileName) throws MissingResourceException {
+        ResourceBundle rb = ResourceBundle.getBundle(fileName);
         Enumeration<String> keys = rb.getKeys();
         String key;
-        double result = 0.0;
+        BigDecimal sum = new BigDecimal("0.0");
         int errorLines = 0;
         while (keys.hasMoreElements()) {
             key = keys.nextElement();
-            Pattern p1 = Pattern.compile(REGEX_FOR_INDEX_WORD);
-            Matcher m1 = p1.matcher(key);
-            if (m1.find()) {
+            if (key.matches(REGEX_FOR_INDEX_WORD)) {
                 try {
                     String j = rb.getString(key);
-                    System.out.println("line=" + key + "=" + rb.getString(key));
                     if (key.matches(REGEX_FOR_INDEX_LINE) && j.matches(REGEX_FOR_INDEX_VALUE)) {
                         String i = key.substring(INDEX.length());
                         String valueIJ = i + j;
-                        System.out.println(valueIJ);
-                        System.out.println("=" + rb.getString(VALUE + valueIJ));
-                        result += Double.parseDouble(rb.getString(VALUE + valueIJ));
+                        sum = sum.add(BigDecimal.valueOf(Double.parseDouble(rb.getString(VALUE + valueIJ))));
                     } else {
                         errorLines++;
-                        System.out.println("Error lines = " + errorLines);
                     }
                 } catch (NumberFormatException | MissingResourceException e) {
                     errorLines++;
-                    System.out.println("Error lines = " + errorLines);
                 }
             }
         }
-        System.out.println(RESULT + result);
-        System.out.println(errorLines);
-        return errorLines;
+        return new Result(sum, errorLines);
+    }
+
+    static class Result {
+        private BigDecimal sum;
+        private int errorLines;
+
+        public Result() {
+
+        }
+
+        public Result(BigDecimal sum, int errorLines) {
+            this.sum = sum;
+            this.errorLines = errorLines;
+        }
+
+        public BigDecimal getSum() {
+            return sum;
+        }
+
+        public int getErrorLines() {
+            return errorLines;
+        }
     }
 
     @Test
-    public void testMainScenario() throws FileNotFoundException {
-        int d = getResult();
+    public void testMainScenarioIn1() throws MissingResourceException {
+        String fileName = "in1";
+        getResult(fileName);
+        int errorLines = getResult(fileName).getErrorLines();
+        Assert.assertEquals(3, errorLines);
+        BigDecimal sum = getResult(fileName).getSum();
+        Assert.assertEquals(new BigDecimal("8.24"), sum);
+    }
+
+    @Test
+    public void testMainScenarioIn2() throws MissingResourceException {
+        String fileName = "in2";
+        getResult(fileName);
+        int errorLines = getResult(fileName).getErrorLines();
+        Assert.assertEquals(9, errorLines);
+        BigDecimal sum = getResult(fileName).getSum();
+        Assert.assertEquals(new BigDecimal("30.242"), sum);
+    }
+
+    @Test
+    public void testMainScenarioIn3() throws MissingResourceException {
+        String fileName = "in3";
+        getResult(fileName);
+        int errorLines = getResult(fileName).getErrorLines();
+        Assert.assertEquals(0, errorLines);
+        BigDecimal sum = getResult(fileName).getSum();
+        Assert.assertEquals(new BigDecimal("1.9"), sum);
+    }
+
+    @Test(expected = MissingResourceException.class)
+    public void testWrongFileName() throws MissingResourceException {
+        String fileName = "in23";
+        getResult(fileName);
     }
 }
