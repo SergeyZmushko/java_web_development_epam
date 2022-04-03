e9bba1e0605d04ab8880bb8382ae16dbf7d3067b
SiarheiZmushka <szmushko83@gmail.com>
2022-04-03 11:12:00 +0300

    collections2_3

--- collections2/src/Runner.java
+++ collections2/src/Runner.java
@@ -1,4 +1,3 @@
-import by.epam.lab.bean.NumLen;
 import by.epam.lab.enums.RoundMethod;
 import by.epam.lab.utils.Utils;
 
@@ -10,22 +9,32 @@ import java.util.*;
 
 public class Runner {
     public static void main(String[] args) {
-        Set<NumLen> setNumLen = new HashSet<>();
         try (Scanner sc = new Scanner(new FileReader(FILE_NAME))) {
+            List<int[]> numLenList = new ArrayList<>();
             while (sc.hasNext()) {
                 String[] mas = sc.nextLine().split(REGEX_SPLIT);
-                NumLen numLen = new NumLen(Utils.lengthCalculate(mas[1], mas[2], mas[3], mas[4], RoundMethod.ROUND, 0));
-                setNumLen.add(numLen);
+                int len = Utils.lengthCalculate(mas[1], mas[2], mas[3], mas[4], RoundMethod.ROUND, 0);
+                int[] numLen = new int[2];
+                numLen[0] = len;
+                numLen[1] = 1;
+                numLenList.add(numLen);
+                for (int i = 0; i <= numLenList.size() - 1; i++) {
+                    for (int j = i + 1; j < numLenList.size(); j++) {
+                        if (numLenList.get(i)[0] == numLenList.get(j)[0]) {
+                            numLenList.get(i)[1]++;
+                            numLenList.remove(numLenList.get(j));
+                        }
+                    }
+                }
             }
-            List<NumLen> listNumLen = new ArrayList<>(setNumLen);
-            Collections.sort(listNumLen, new Comparator<NumLen>() {
+            Collections.sort(numLenList, new Comparator<int[]>() {
                 @Override
-                public int compare(NumLen o1, NumLen o2) {
-                    return o2.getNum() - o1.getNum();
+                public int compare(int[] o1, int[] o2) {
+                    return o2[1] - o1[1];
                 }
             });
-            for (NumLen element : listNumLen){
-                System.out.println(element);
+            for (int[] element : numLenList) {
+                System.out.println(Arrays.toString(element));
             }
         } catch (FileNotFoundException e) {
             System.out.println(FILE_NOT_FOUND);
