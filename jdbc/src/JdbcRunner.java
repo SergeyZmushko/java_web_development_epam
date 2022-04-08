883a02a6fb0eaa83b893676e324e138ed1e58e3a
SiarheiZmushka <szmushko83@gmail.com>
2022-04-08 18:29:22 +0300

    jdbc1_2

--- jdbc/src/JdbcRunner.java
+++ jdbc/src/JdbcRunner.java
@@ -9,35 +9,28 @@ import java.util.List;
 public class JdbcRunner {
 
     public static void main(String[] args) {
-        try {
-            ResultSet rs = null;
-            try (Connection cn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-                 Statement st = cn.createStatement();
-                 PreparedStatement ps = cn.prepareStatement(SQL_INSERT_INTO_FREQUENCIES)) {
-                st.executeUpdate(SQL_DELETE_FROM_FREQUENCIES);
-                rs = st.executeQuery(SQL_SELECT_LEN_NUM);
-                //1
+
+        try (Connection cn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+             Statement st = cn.createStatement();
+             PreparedStatement ps = cn.prepareStatement(SQL_INSERT_INTO_FREQUENCIES)) {
+            st.executeUpdate(SQL_DELETE_FROM_FREQUENCIES);
+            try (ResultSet rs = st.executeQuery(SQL_SELECT_LEN_NUM)) {
                 List<LenNum> lenNumList = new ArrayList<>();
                 while (rs.next()) {
                     LenNum lenNum = new LenNum(rs.getInt(LEN_IND), rs.getInt(NUM_IND));
                     lenNumList.add(lenNum);
                     System.out.println(lenNum);
                 }
-                //3
                 for (LenNum lenNum : lenNumList) {
                     ps.setInt(LEN_IND, lenNum.getLen());
                     ps.setInt(NUM_IND, lenNum.getNum());
                     ps.addBatch();
                 }
                 ps.executeBatch();
-
-                rs = st.executeQuery(SQL_SELECT_FROM_FREQUENCIES_LEN_MORE_NUM);
+            }
+            try (ResultSet rs = st.executeQuery(SQL_SELECT_FROM_FREQUENCIES_LEN_MORE_NUM)) {
                 while (rs.next()) {
                     System.out.println(rs.getInt(LEN_IND) + SPLITTER + rs.getInt(NUM_IND));
-                }
-            } finally {
-                if (rs != null && !rs.isClosed()) {
-                    rs.close();
                 }
             }
         } catch (SQLException e) {
--- jdbc/src/by/epam/lab/utils/Constants.java
+++ jdbc/src/by/epam/lab/utils/Constants.java
@@ -3,9 +3,9 @@ package by.epam.lab.utils;
 public class Constants {
     public final static String USER = "root";
     public final static String PASSWORD = "root";
-    public final static String DB_URL = "jdbc:mysql://127.0.0.1:3306/segments?serverTimezone=Europe/Minsk";
+    public final static String DB_URL = "jdbc:mysql://127.0.0.1:3306/segments";
     public final static String SQL_SELECT_LEN_NUM = "SELECT round(abs(x2 - x1)) AS len, COUNT(*) AS num" +
-            " FROM coordinates GROUP BY 1 ORDER BY 1";
+            " FROM coordinates GROUP BY len ORDER BY len";
     public final static String SQL_DELETE_FROM_FREQUENCIES = "DELETE FROM frequencies";
     public final static String SQL_INSERT_INTO_FREQUENCIES = "INSERT INTO frequencies (len, num) VALUES (?, ?)";
     public final static String SQL_SELECT_FROM_FREQUENCIES_LEN_MORE_NUM = "SELECT len, num FROM frequencies WHERE len>num";
